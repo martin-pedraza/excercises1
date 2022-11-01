40d7daaf622245e57548abdc6312ec5f8d4e7a3f
martin-pedraza <pedraza.martindiego@gmail.com>
2022-11-01 08:15:43 -0300

    class01-exI01

---
+++ class01-exI01/class01-exI01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace class01_exI01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumadoNumeroIngresado = 0;
+            int numeroIngresado;
+            int numeroIteraciones = 5;
+            int limiteMaximo = int.MinValue;
+            int limiteMiinimo = int.MaxValue;
+            for (int i = 1; i <= numeroIteraciones; i++)
+            {
+                do
+                {
+                    Console.WriteLine($"Ingrese un numero: [{i}]");
+                } while (!int.TryParse(Console.ReadLine(), out numeroIngresado));
+                if (numeroIngresado > limiteMaximo)
+                {
+                    limiteMaximo = numeroIngresado;
+                }
+                if (numeroIngresado < limiteMiinimo)
+                {
+                    limiteMiinimo = numeroIngresado;
+                }
+                sumadoNumeroIngresado += numeroIngresado;
+            }
+            Console.WriteLine($"Promedio de los numeros ingresados: {sumadoNumeroIngresado/numeroIteraciones}");
+            Console.WriteLine($"Menor numero ingresado: {limiteMiinimo}");
+            Console.WriteLine($"Mayor numero ingresado: {limiteMaximo}");
+        }
+    }
+}
